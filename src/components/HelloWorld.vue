a2fdfbe6fab9dfe9ce46c9db211af84969f3eab2
Tlek Suyerbassov <tleksuerbasov@mail.ru>
2021-01-18 23:04:55 +0600

    modified linking page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="hello">
-      <router-link to="/chat" target="_blank" style="margin-right:15px;" rel="noopener">Ваши чат-боты</router-link>
-      <router-link to="/balans" target="_blank" style="margin-right:15px;" rel="noopener">Ваш баланс</router-link>
-      <router-link to="/tarif" target="_blank" style="margin-right:15px;" rel="noopener">Тарифы</router-link>
+      <router-link to="/chat" style="margin-right:15px;">Ваши чат-боты</router-link>
+      <router-link to="/balans" style="margin-right:15px;">Ваш баланс</router-link>
+      <router-link to="/tarif" style="margin-right:15px;">Тарифы</router-link>
   </div>
 </template>
 
